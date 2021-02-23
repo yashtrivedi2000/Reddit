0e9bc8e3cbe432363bd3c216bb1ff26e9d3b0b09
Yash <yashtrivedi2000@outlook.com>
2021-02-23 23:33:18 +0530

    create and update post added

--- dist/resolvers/post.js
+++ dist/resolvers/post.js
@@ -11,6 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PostResolver = void 0;
 const Post_1 = require("../entities/Post");
@@ -22,6 +31,24 @@ let PostResolver = class PostResolver {
     }
     postFind(id, { em }) {
         return em.findOne(Post_1.Post, { _id: id });
+    }
+    createPost(title, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = em.create(Post_1.Post, { title });
+            yield em.persistAndFlush(post);
+            return post;
+        });
+    }
+    updatePost(_id, title, { em }) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = yield em.findOne(Post_1.Post, { _id });
+            if (!post || typeof _id === null) {
+                return null;
+            }
+            post.title = title;
+            yield em.persistAndFlush(post);
+            return post;
+        });
     }
 };
 __decorate([
@@ -39,6 +66,22 @@ __decorate([
     __metadata("design:paramtypes", [Number, Object]),
     __metadata("design:returntype", Promise)
 ], PostResolver.prototype, "postFind", null);
+__decorate([
+    type_graphql_1.Mutation(() => Post_1.Post),
+    __param(0, type_graphql_1.Arg("title", () => String)),
+    __param(1, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "createPost", null);
+__decorate([
+    type_graphql_1.Mutation(() => Post_1.Post),
+    __param(0, type_graphql_1.Arg("id")), __param(1, type_graphql_1.Arg("title")),
+    __param(2, type_graphql_1.Ctx()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number, String, Object]),
+    __metadata("design:returntype", Promise)
+], PostResolver.prototype, "updatePost", null);
 PostResolver = __decorate([
     type_graphql_1.Resolver()
 ], PostResolver);
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,7 +1,7 @@
 
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import {  Arg, Ctx, Int, Resolver } from "type-graphql";
+import {  Arg, Ctx, Int, Mutation, Resolver } from "type-graphql";
 import { Query } from "type-graphql";
 
 @Resolver()
@@ -19,5 +19,28 @@ export class PostResolver{
         @Ctx() {em}:MyContext): Promise<Post | null>
     {
         return em.findOne(Post,{_id:id});
+    }
+
+    @Mutation(()=>Post)
+    async createPost(@Arg("title",()=>String) title:string,
+        @Ctx() {em}:MyContext): Promise<Post | null>
+    {
+        const post = em.create(Post,{title});
+        await em.persistAndFlush(post);
+        return post; 
+    }
+
+    @Mutation(()=>Post)
+    async updatePost(@Arg("id") _id:number,@Arg("title") title:string,
+        @Ctx() {em}:MyContext): Promise<Post | null>
+    {
+        const post = await em.findOne(Post,{_id});
+        if(!post || typeof _id===null){
+            return null;
+        }
+        post.title=title;
+        await em.persistAndFlush(post);
+        return post;
+        
     }
 }
